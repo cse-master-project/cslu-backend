a272085d0e22240630a5ad329d373c647d3d51e3
minyoung555 <mindud02@naver.com>
2024-03-08 14:25:27 +0900

    init

---
+++ src/main/java/com/example/csemaster/CseMasterApplication.java
@@ -0,0 +1,15 @@
+package com.example.csemaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CseMasterApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CseMasterApplication.class, args);
+        System.out.println("hello world!!");
+    }
+
+}
