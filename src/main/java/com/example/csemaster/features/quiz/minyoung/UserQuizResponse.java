4d0f16465fe7d67d2ec5a26069041cb4ffd90cfe
minyoung555 <mindud02@naver.com>
2024-03-28 15:39:37 +0900

    UserQuizResponse 형식 변경

--- src/main/java/com/example/csemaster/features/quiz/minyoung/UserQuizResponse.java
+++ src/main/java/com/example/csemaster/features/quiz/minyoung/UserQuizResponse.java
@@ -12,24 +12,16 @@ public class UserQuizResponse {
     private Long quizId;
     private String subject;
     private String detailSubject;
-    private double correctRate;
     private String jsonContent;
     private LocalDateTime createAt;
-    private Boolean hasImage;
-    private Boolean isDeleted;
-    private Long userQuizId;
     private Integer permissionStatus;
 
     public UserQuizResponse(QuizEntity quiz, UserQuizEntity user) {
         this.quizId = quiz.getQuizId();
         this.subject = quiz.getSubject();
         this.detailSubject = quiz.getDetailSubject();
-        this.correctRate = quiz.getCorrectRate();
         this.jsonContent = quiz.getJsonContent();
         this.createAt = quiz.getCreateAt();
-        this.hasImage = quiz.getHasImage();
-        this.isDeleted = quiz.getIsDeleted();
-        this.userQuizId = user.getUserQuizId();
         this.permissionStatus = user.getPermissionStatus();
     }
 }
