073d568b5d66883b6e4932c55e899ca900428706
qkrdpwls1223 <yejinn_0819@naver.com>
2024-04-11 14:56:56 +0900

    기능 추가: 유저 토큰 재발급 추가

--- src/main/java/com/example/csemaster/jwt/JwtProvider.java
+++ src/main/java/com/example/csemaster/jwt/JwtProvider.java
@@ -145,6 +145,13 @@ public class JwtProvider {
         return new UsernamePasswordAuthenticationToken(principal, "", authorities);
     }
 
+    // 토큰에서 id만 추출
+    public String getSubject(String refreshToken) {
+        Claims claims = parseClaims(refreshToken);
+
+        return claims.getSubject();
+    }
+
     // 토큰 정보를 검증하는 메서드
     public boolean validateToken(String token) {
         try {
