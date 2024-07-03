b53ad274c65ef64e0581fb809a2c206d034812ac
qkrdpwls1223 <yejinn_0819@naver.com>
2024-07-03 12:33:48 +0900

    요청 바디 출력 디버깅 코드 추가

--- src/main/java/com/example/csemaster/LoggingFilter.java
+++ src/main/java/com/example/csemaster/LoggingFilter.java
@@ -7,6 +7,8 @@ import org.slf4j.MDC;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.UUID;
 
 @Slf4j
@@ -22,7 +24,23 @@ public class LoggingFilter implements Filter {
         MDC.put("traceId", requestID);
         try {
             log.info("Request URI : '{}', from IP : {}", requestURI, clientIP);
+            try {
+                // 1. ServletInputStream 객체 얻기
+                InputStream inputStream = request.getInputStream();
 
+                // 2. 바이트 배열로 요청 바디 읽기
+                byte[] buffer = new byte[4096];
+                int bytesRead;
+                StringBuilder requestBody = new StringBuilder();
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    requestBody.append(new String(buffer, 0, bytesRead));
+                }
+
+                // 3. 바이트 배열을 문자열로 변환하여 출력
+                log.debug("Request Body: " + requestBody);
+            } catch (IOException e) {
+                log.debug(e.getMessage());
+            }
             chain.doFilter(request, response);
         } catch (Exception e) {
             log.error(e.getMessage(), e);
