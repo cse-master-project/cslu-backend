88b7aa8666070cfb031a4f141bda6c88e225e23f
qkrdpwls1223 <yejinn_0819@naver.com>
2024-07-04 09:30:19 +0900

    문제 신고 가져올 시 userId를 노출하는 문제 해결

--- src/main/java/com/example/csemaster/dto/response/QuizReportResponse.java
+++ src/main/java/com/example/csemaster/dto/response/QuizReportResponse.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 public class QuizReportResponse {
     private Long quizReportId;
     private Long quizId;
-    private String userId;
+    private String userNickname;
     private String content;
     private LocalDateTime reportAt;
 }
