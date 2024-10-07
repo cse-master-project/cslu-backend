9241f708bda31975e5bbdc2292785bed57febfff
qkrdpwls1223 <yejinn_0819@naver.com>
2024-10-08 06:07:11 +0900

    수정 : 과목 추가 에러 수정(생성자에서 생긴 오류)

--- src/main/java/com/example/csemaster/v2/dto/SubjectRequest.java
+++ src/main/java/com/example/csemaster/v2/dto/SubjectRequest.java
@@ -4,9 +4,15 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.beans.ConstructorProperties;
+
 @Data
-@AllArgsConstructor
 public class SubjectRequest {
     @NotBlank
     private String subject;
+
+    @ConstructorProperties({"subject"})
+    public SubjectRequest(String subject) {
+        this.subject = subject;
+    }
 }
